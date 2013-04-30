0cbb472f226641f77c35db1a183fdcca88053a6e
Dolugen <dolugen@gmail.com>
2013-04-30 23:06:07 +0800

    Header link for volunteer page.

--- forum_modules/volunteer/startup.py
+++ forum_modules/volunteer/startup.py
@@ -5,3 +5,7 @@ from django.utils.translation import ugettext as _
 ui.register(ui.FOOTER_LINKS,
     ui.Link(_('volunteer'), '/volunteer'),
 )
+
+ui.register(ui.HEADER_LINKS,
+    ui.Link(_('volunteer'), '/volunteer'),
+)
